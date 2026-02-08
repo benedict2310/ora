679bb25773b2bd523a5c7e2acf913ba21872c55b
Benedict Bleimschein <benedict.bleimschein@gmail.com>
2026-02-08 22:50:36 +0100

    feat(preferences): add provider configuration tab

--- Ora/Preferences/PreferencesWindow.swift
+++ Ora/Preferences/PreferencesWindow.swift
@@ -30,6 +30,8 @@ struct PreferencesWindow: View {
                 switch coordinator.selectedTab {
                 case .general:
                     GeneralPreferencesView()
+                case .providers:
+                    ProviderPreferencesView()
                 case .models:
                     ModelsPreferencesView()
                 case .permissions:
